3424678bff97af468ca7cda9f882789f45671dd4
Leo Gallucci <elgalu3@gmail.com>
2022-04-25 21:46:05 +0200

    upgrades
    Signed-off-by: Leo Gallucci <elgalu3@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# elgalu/ensure setup/bootstrap script
+# setup/bootstrap script
 #   Script to idempotently validate that Python, PyEnv, Poetry, PyInvoke, venv,
 #   Git, Docker, ssh-client, etc are installed on Linux and OSX
 #
@@ -74,6 +74,9 @@ if [ "$(uname)" == 'Darwin' ]; then
     export _tail='gtail'
     export _date='gdate'
     export _timeout='gtimeout'
+    if ! command -v gdate 1>/dev/null 2>&1; then
+        util.die "GNU date is not installed, run: brew install coreutils"
+    fi
 elif [ "$(uname)" == 'Linux' ]; then
     export _tee='tee'
     export _cut='cut'
@@ -119,12 +122,12 @@ ensure_git_submodules
 #--------------#
 ## Ensure docker client is installed and that doesn't require sudo.
 function ensure_docker() {
-    if ! command -v docker --version 1>/dev/null; then
+    if ! command -v docker --version 1>/dev/null 2>&1; then
         util.log.error "it seems docker is not installed, please install it first."
         docker --version
     fi
 
-    if ! command -v docker ps --last=1 1>/dev/null; then
+    if ! command -v docker ps --last=1 1>/dev/null 2>&1; then
         util.log.error "it seems docker is installed but it requires sudo, fix that first."
         docker ps --last=1
     fi
@@ -137,16 +140,16 @@ ensure_docker
 #------------------------------------#
 ## Ensure ssh client is installed.
 function ensure_ssh_client() {
-    if ! command which ssh 1>/dev/null; then
+    if ! command which ssh 1>/dev/null 2>&1; then
         util.die "it seems ssh is not installed, please install a tool like openssh-client."
     fi
-    if ! command which ssh-keygen 1>/dev/null; then
+    if ! command which ssh-keygen 1>/dev/null 2>&1; then
         util.die "it seems ssh-keygen is not installed, please install a tool like openssh-client."
     fi
-    if ! command which ssh-copy-id 1>/dev/null; then
+    if ! command which ssh-copy-id 1>/dev/null 2>&1; then
         util.die "it seems ssh-copy-id is not installed, please install a tool like openssh-client."
     fi
-    if ! command which sftp 1>/dev/null; then
+    if ! command which sftp 1>/dev/null 2>&1; then
         util.die "it seems sftp is not installed."
     fi
 }
@@ -157,11 +160,11 @@ ensure_ssh_client
 #--- Ensure we have grep, awk, etc ---#
 #-------------------------------------#
 function ensure_system_helpers() {
-    if ! command grep --version 1>/dev/null; then
+    if ! command grep --version 1>/dev/null 2>&1; then
         util.die "it seems grep is not installed, please install it."
     fi
-    if ! command awk --version 1>/dev/null; then
-        util.die "it seems awk is not installed, please install it."
+    if ! command mawk -W version 1>/dev/null 2>&1; then
+        util.die "it seems mawk is not installed, please install it, e.g. brew install mawk"
     fi
 }
 
@@ -172,6 +175,7 @@ ensure_system_helpers
 #-----------------------------#
 ## Installs PyEnv in current user's home, rootless.
 function install_pyenv() {
+    util.log.info "Installing PyEnv..."
     checkout "${GITHUB}/pyenv/pyenv.git" "${PYENV_ROOT}"
     checkout "${GITHUB}/pyenv/pyenv-doctor.git" "${PYENV_ROOT}/plugins/pyenv-doctor"
     checkout "${GITHUB}/pyenv/pyenv-installer.git" "${PYENV_ROOT}/plugins/pyenv-installer"
@@ -183,7 +187,7 @@ function install_pyenv() {
 ## Validates PyEnv is working
 # https://github.com/pyenv/pyenv
 function validate_pyenv() {
-    if ! command -v pyenv 1>/dev/null; then
+    if ! command -v pyenv 1>/dev/null 2>&1; then
         util.log.error "it seems you still have not added 'pyenv' to the load path."
 
         { # Without args, `init` commands print installation help
@@ -216,7 +220,7 @@ function ensure_python_version() {
         util.log.info "PyEnv seems to be installed already at '${PYENV_ROOT}'."
         validate_pyenv
         pyenv_ensure_required_python_version
-    elif [ "${USER}" == "root" ]; then
+    elif [ "${USER}" == "root" ] || [ "${USER}" == "" ]; then
         util.log.info "We're running as root, will skip installing PyEnv."
         if python --version | grep "$(cat .python-version)"; then
             util.log.info "We're already running the required version of Python."
@@ -269,7 +273,7 @@ source ".venv/bin/activate" || util.die "Failed to activate the virtual environm
 ## https://github.com/python-poetry/poetry
 function ensure_poetry() {
     # Do not install poetry within your virtual environment
-    if command -v poetry --version 1>/dev/null; then
+    if command -v poetry --version 1>/dev/null 2>&1; then
         if [ -z "${CDP_BUILD_VERSION}" ]; then
             poetry self update || util.die "Failed to self update poetry. Quitting..."
         else
@@ -277,7 +281,7 @@ function ensure_poetry() {
         fi
     else
         util.log.error "Please install poetry yourself, outside this project's environment, e.g."
-        util.log.error "curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -"
+        util.log.error "curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -"
         exit 20
     fi
     # --no-root: Do not install the root package (your project).
@@ -314,13 +318,13 @@ ensure_github_cli_releases
 ## https://github.com/koalaman/shellcheck
 function ensure_shellcheck_if_needed() {
     if test -n "$(find . -maxdepth 3 -name '*.sh' -print -quit)"; then
-        if ! command -v shellcheck --version 1>/dev/null; then
+        if ! command -v shellcheck --version 1>/dev/null 2>&1; then
             util.log.error "Shell/Bash found on this project but shellcheck is not installed."
-            util.log.error "https://github.com/koalaman/shellcheck"
-            util.log.error "MacOS: brew install shellcheck"
-            util.log.error "Linux: snap install --channel=edge shellcheck"
-            util.log.error "Linux: sudo apt install shellcheck"
-            util.log.error "AnyOS: pip install shellcheck-py"
+            util.log.error "  https://github.com/koalaman/shellcheck"
+            util.log.error "  MacOS: brew install shellcheck"
+            util.log.error "  Linux: snap install --channel=edge shellcheck"
+            util.log.error "  Linux: sudo apt install shellcheck"
+            util.log.error "  AnyOS: pip install shellcheck-py"
             exit 35
         fi
     else
@@ -336,7 +340,7 @@ ensure_shellcheck_if_needed
 ## https://github.com/hadolint/hadolint
 function ensure_hadolint_if_needed() {
     if test -n "$(find . -maxdepth 3 -name 'Dockerfile*' -print -quit)"; then
-        if ! command -v hadolint --version 1>/dev/null; then
+        if ! command -v hadolint --version 1>/dev/null 2>&1; then
             util.log.error "Dockerfile(s) found on this project but hadolint is not installed."
             util.log.error "MacOS: brew install hadolint"
             util.log.error "https://github.com/hadolint/hadolint#install"
@@ -356,7 +360,7 @@ ensure_hadolint_if_needed
 function ensure_kube_linter_if_needed() {
     # If there are yaml files containing both `apiVersion:` and `kind:` then is very likely this project contains K8s manifests
     if test -n "$(find . -name '*.yaml' -type f -exec grep -qlm1 'apiVersion:' {} \; -exec grep -lm1 -H 'kind:' {} \; -a -quit)"; then
-        if ! command -v kube-linter version 1>/dev/null; then
+        if ! command -v kube-linter version 1>/dev/null 2>&1; then
             util.log.error "Kubernetes manifest(s) found on this project but kube-linter is not installed."
             util.log.error "MacOS: brew install kube-linter"
             util.log.error "Linux: GO111MODULE=on go install golang.stackrox.io/kube-linter/cmd/kube-linter@latest"
@@ -364,7 +368,7 @@ function ensure_kube_linter_if_needed() {
             exit 42
         fi
         # KubeAudit doesn't lint, focuses more on security of running clusters
-        # if ! command -v kubeaudit version 1>/dev/null; then
+        # if ! command -v kubeaudit version 1>/dev/null 2>&1; then
         #     util.log.error "Kubernetes manifest(s) found on this project but kubeaudit is not installed."
         #     util.log.error "MacOS: brew install kubeaudit"
         #     util.log.error "Linux: https://github.com/Shopify/kubeaudit/releases"
@@ -372,7 +376,7 @@ function ensure_kube_linter_if_needed() {
         # fi
         #
         # KubeScore doesn't work with CDP templating system, yields errors like `Invalid value: "{{{APPLICATION_ID}}}"`
-        # if ! command -v kube-score version 1>/dev/null; then
+        # if ! command -v kube-score version 1>/dev/null 2>&1; then
         #     util.log.error "Kubernetes manifest(s) found on this project but kube-score is not installed."
         #     util.log.error "MacOS: brew install kube-score"
         #     util.log.error "Linux: https://github.com/zegl/kube-score/releases"
@@ -380,7 +384,7 @@ function ensure_kube_linter_if_needed() {
         # fi
         #
         # Polaris doesn't work with CDP templating system and fails without outputting offending lines
-        # if ! command -v polaris version 1>/dev/null; then
+        # if ! command -v polaris version 1>/dev/null 2>&1; then
         #     util.log.error "Kubernetes manifest(s) found on this project but polaris is not installed."
         #     util.log.error "MacOS: brew tap FairwindsOps/tap && brew install FairwindsOps/tap/polaris"
         #     util.log.error "Linux: https://github.com/fairwindsops/polaris/releases"
@@ -454,11 +458,11 @@ function ensure_pyinvoke_if_needed() {
             util.die "Failed to poetry run invoke setup"
         fi
         # let's make sure we don't need `poetry run` since the .venv is activated anyways
-        if ! invoke tests; then
-            util.die "Failed to run tests"
-        fi
         if ! invoke hooks; then
             util.die "Failed to run hooks"
+        fi
+        if [ -d 'tests' ] && ! invoke tests; then
+            util.die "Failed to run tests"
         fi
     else
         util.log.info "This project doesn't seem to be using pyinvoke."
@@ -466,6 +470,31 @@ function ensure_pyinvoke_if_needed() {
 }
 
 ensure_pyinvoke_if_needed
+
+#-----------------------------------------#
+#--- Ensure NodeJS for MkDocs JS needs ---#
+#-----------------------------------------#
+## https://github.com/DavidAnson/markdownlint
+function ensure_nodejs_if_needed() {
+    if [ -f "mkdocs.yml" ] || [ -f "docs/robots.txt" ]; then
+        if ! command node --version 1>/dev/null 2>&1; then
+            util.log.error "Please install NodeJS or put it in the PATH."
+            util.log.error "  MacOS: brew install node"
+            util.log.error "  Linux: apt -qyy install nodejs"
+            exit 54
+        fi
+        if ! command npm --version 1>/dev/null 2>&1; then
+            util.log.error "Please install NPM or put it in the PATH."
+            util.log.error "  MacOS: brew install npm"
+            util.log.error "  Linux: apt -qyy install npm"
+            exit 55
+        fi
+    else
+        util.log.info "This project doesn't seem to need NodeJS."
+    fi
+}
+
+ensure_nodejs_if_needed
 
 #------------#
 #--- DONE ---#
